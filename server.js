45d80aa96e258db28905533bb8c8baca537eda06
njheymann <nils.heymann@Hotmail.com>
2023-09-07 17:57:25 +1000

    update bycrypt

--- server.js
+++ server.js
@@ -1,70 +1,52 @@
+// Express.js server setup
 require("dotenv").config();
-
 const path = require("path");
 const express = require("express");
-
-const session = require("express-session"); // import express-session
+const session = require("express-session");
 const exphbs = require("express-handlebars");
-const sequelize = require("./config/connection");
 const routes = require("./controllers");
 
+const helpers = require("./utils/auth.js");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const app = express();
+const PORT = process.env.PORT || 3030;
 
-const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 
-const hbs = exphbs.create({
-  helpers: {
-    compare: function (variableOne, comparator, variableTwo) {
-      if (eval(variableOne + comparator + variableTwo)) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    formatDate: function (date) {
-      // Format the date to format "DD-MM-YYYY"
-      const dateData = date.toISOString().split("T")[0].split("-");
-      return dateData[2] + "/" + dateData[1] + "/" + dateData[0];
-    },
-  },
-}).engine;
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-app.engine("handlebars", hbs);
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// Use express-session middleware
+app.use(express.json());
 app.use(
-  session({
-    secret: "your_secret_key", // this secret will be used for signing the session ID cookie. Change it to your own secret string.
-    resave: false, // force the session to be saved back to the session store
-    saveUninitialized: false, // save uninitialized session to the store
-    cookie: {
-      secure: "auto",
-      httpOnly: true,
-      maxAge: 72 * 60 * 60 * 1000, // 72 hours in milliseconds
-    }, // automatically set the cookie as secure if the request is secure
+  express.urlencoded({
+    extended: true,
   })
 );
-
-// middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(routes);
 
-// Error-handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
-
 sequelize
-  .sync({ force: false })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+  .sync({
+    force: false,
   })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Now listening on http://localhost:3030/home")
+    );
   });
