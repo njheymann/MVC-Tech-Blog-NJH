4b64938fb910b4006da09ab872e15141f4ee04c2
njheymann <nils.heymann@Hotmail.com>
2023-09-09 09:24:33 +1000

    fix??

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,7 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    process.env.JAWSDB_URL
-  );
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
